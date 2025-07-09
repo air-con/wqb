5bd9cda6bebf4a8dc7a8dcb38d5341ec9a637298
Rosalind Louise <144047708+air-con@users.noreply.github.com>
2025-07-09 10:17:37 +0800

    Update celeryconfig.py

--- celeryconfig.py
+++ celeryconfig.py
@@ -15,7 +15,7 @@ task_annotations = {
     'wqb.tasks.simulate_alpha_task': {
         # 不设置 rate_limit，让任务跑满卡槽
         'soft_time_limit': None,
-        'time_limit': 600,       # 10分钟硬超时，强制终止
+        'time_limit': 1200,       # 20分钟硬超时，强制终止
         # 'retry_policy': {
         #     'max_retries': 2,
         #     'interval_start': 60,
@@ -24,7 +24,7 @@ task_annotations = {
     },
     'wqb.tasks.simulate_single_alpha_task': {
         'soft_time_limit': None,
-        'time_limit': 600,       # 3分钟硬超时
+        'time_limit': 1200,       # 20分钟硬超时
         # 'retry_policy': {
         #     'max_retries': 3,
         #     'interval_start': 30,
